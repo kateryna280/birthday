a0bb54eae46778f242be2576974bc19b2738ce9b
kate <kate.druzghko@gmail.com>
2026-06-02 01:01:59 +0200

    цц

---
+++ html/script.js
@@ -0,0 +1,49 @@
+function checkPassword(){
+
+  const password =
+  document.getElementById('passwordInput').value;
+
+  if(password === '31032025'){
+
+    document.getElementById('passwordScreen')
+    .style.display = 'none';
+
+    document.getElementById('mainSite')
+    .classList.remove('hidden');
+
+  }else{
+
+    document.getElementById('errorText')
+    .innerText =
+    'неправильный пароль... это важная дата для вас';
+
+  }
+
+}
+
+function playMusic(){
+
+  const audio =
+  document.getElementById('musicAudio');
+
+  audio.play();
+
+}
+
+function showGuests(){
+
+  document.getElementById('guests')
+  .classList.remove('hidden');
+
+}
+
+function blowCandle(){
+
+  const wish =
+  document.getElementById('wishInput').value;
+
+  console.log(wish);
+
+  window.location.href = 'dark.html';
+
+}
